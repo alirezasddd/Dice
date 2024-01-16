dc836879c52166709629532cacca0a9891d8e596
Alireza <alireza.sad.ger@gmail.com>
2024-01-16 04:09:32 +0330

    some variable changed

--- dice.py
+++ dice.py
@@ -2,24 +2,24 @@ import random
 import winsound
 
 
-Sask = input(""" are you ready? 
+ready = input(""" are you ready? 
      enter Y for yes and N for no
      >>>""")
 
 
-if Sask.lower() == "y":
-    Fask = input("""wanna play Snakes and Ladders or Backgammon? """)
+if ready.lower() == "y":
+    ask = input("""wanna play Snakes and Ladders or Backgammon? """)
     winsound.Beep(random.randint(50, 500), random.randint(100, 1000))
 
-    if Fask == "Snakes and Ladders":
+    if ask == "Snakes and Ladders":
         print(random.randint(1, 6))
         winsound.Beep(1500, 250)
 
-    elif Fask == "Backgammon":
+    elif ask == "Backgammon":
         print(random.choices([1, 2, 3, 4, 5, 6], k=2))
         winsound.Beep(1000, 200)
 
 
-elif Sask.lower() == "n":
+elif ready.lower() == "n":
     print("allright. goodluck")
     winsound.Beep(random.randint(50, 2000), random.randint(200, 800))

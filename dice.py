aaf61186c7255907fafdf8fa7c85d8736edf3120
Alireza <alireza.sad.ger@gmail.com>
2024-01-16 04:01:00 +0330

    the dice has sound

--- dice.py
+++ dice.py
@@ -1,13 +1,25 @@
 import random
+import winsound
 
 
-ask = input("""are you ready? 
+Sask = input(""" are you ready? 
      enter Y for yes and N for no
      >>>""")
 
-if ask.lower() == "y":
-     print(random.randint(1, 6))
 
-elif ask.lower() == "n":
-     print("allright. goodluck")
+if Sask.lower() == "y":
+    Fask = input("""wanna play Snakes and Ladders or Backgammon? """)
+    winsound.Beep(random.randint(50, 500), random.randint(100, 1000))
 
+    if Fask == "Snakes and Ladders":
+        print(random.randint(1, 6))
+        winsound.Beep(1500, 250)
+
+    elif Fask == "Backgammon":
+        print(random.choices([1, 2, 3, 4, 5, 6], k=2))
+        winsound.Beep(1000, 200)
+
+
+elif Sask.lower() == "n":
+    print("allright. goodluck")
+    winsound.Beep(random.randint(50, 2000), random.randint(200, 800))
